2dc099297dbf61fb77cd371ae344cc6704ca2dc8
mydimons <mydimons.kb@gmail.com>
2026-01-11 22:33:01 -0800

    theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,7 +41,7 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
-html_theme = 'pydata-sphinx-theme'
+html_theme = 'pydata_sphinx_theme'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
